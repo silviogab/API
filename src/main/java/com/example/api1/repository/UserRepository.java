b52a8c1aa61e6a48c172408f67c2438da17ee9eb
Sílvio Gabriel <gabriel.castros789@gmail.com>
2026-02-20 17:12:09 -0500

    Merge remoto

--- src/main/java/com/example/api1/repository/UserRepository.java
+++ src/main/java/com/example/api1/repository/UserRepository.java
@@ -1,6 +1,6 @@
 package com.example.api1.repository;
 
-import com.example.api1.domain.User;  // ⚠ veja se está domain mesmo
+import com.example.api1.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
 
